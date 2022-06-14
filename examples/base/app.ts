9f85dd1084b1f5aa5029583bc358038f680afd7d
guoxl <guoxl@163.com>
2022-06-14 11:14:15 +0800

    chore: 测试数据

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -1,18 +1,15 @@
 import FsSlsTracker from '../../src/index'
 // import FsLogger from 'fshows-logger'
 
-const extra = {
-  mode: '独立收银模式'
-}
 const OPTS = {
-  host: 'cn-hangzhou.log.aliyuncs.com', // 所在地域的服务入口。例如cn-hangzhou.log.aliyuncs.com
-  project: 'fshows-ecs-tes', // Project名称。
-  logstore: 'web-seeds-analysis-dev', // Logstore名称。
+  host: '', // 所在地域的服务入口。例如cn-hangzhou.log.aliyuncs.com
+  project: '', // Project名称。
+  logstore: '', // Logstore名称。
   time: 10, // 发送日志的时间间隔，默认是10秒。
   count: 10 // 发送日志的数量大小，默认是10条。
 }
 let param = {
-  userName: 'gxl测试',
+  userName: '测试',
   userId: '123456',
   userAgent: '测试数据'
 }
